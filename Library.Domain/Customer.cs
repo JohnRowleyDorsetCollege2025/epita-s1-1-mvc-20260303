60666dd0767e806039f86edbed7bcfe757d5e226
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2026-03-03 17:53:53 +0000

    Added model builder

---
+++ Library.Domain/Customer.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Library.Domain;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+
+    public List<Invoice> Invoices { get; set; } = new();
+}
